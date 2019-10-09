9663f10e8407814cb73d440b6b8b8e31382fbf61
Edmon <encrxpt@gmail.com>
2019-10-09 15:43:35 -0500

    Created the publisher model with validations. Also changed some validations to genre and author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ApplicationRecord
 
   validates :name, presence: true
-  validates :name, uniqueness: true
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,4 @@
-class Genre < ApplicationRecord
+class Publisher < ApplicationRecord
 
   validates :name, presence: true
 end
