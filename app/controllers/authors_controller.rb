4004e80ef46acaf29de9da0de0306a342ac15a4d
Edmon <encrxpt@gmail.com>
2019-10-17 11:40:47 -0500

    Created the authors index, showing their name and the books they've written.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,20 @@
+class AuthorsController < ApplicationController
+  def index
+    @pagy, @authors = if params[:name]
+                      pagy(Author.where('name LIKE ?', "%#{params[:name]}%"), items: 10)
+                    else
+                      pagy(Author.all, items: 10)
+    end
+  end
+
+  def show
+    @authors = Author.find(params[:id])
+  end
+
+  private
+
+  def books_params
+    params.require(:authors).permit(:id, :title)
+  end
+
+end
